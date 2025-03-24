c86bf16a48373468d2b7873d945816588a4cc391
pcl <patrice.corteel@gmail.com>
2025-03-24 19:07:22 +0100

    Added code-server extensions

--- .bootstrap-scripts/java-setup.sh
+++ .bootstrap-scripts/java-setup.sh
@@ -1,3 +1,14 @@
+# add extensions
+code-server --install-extension vscjava.vscode-java-pack
+code-server --install-extension bierner.markdown-mermaid
+code-server --install-extension vscjava.vscode-maven
+code-server --install-extension vscjava.vscode-java-dependency
+code-server --install-extension vscjava.vscode-java-test
+code-server --install-extension gabrielbb.vscode-lombok
+code-server --install-extension continue.continue
+    
+
+# install jdh
 sudo apt-get install -y wget apt-transport-https gpg \ 
 && wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null \
 && echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list \
@@ -7,6 +18,7 @@ sudo apt-get install -y wget apt-transport-https gpg \
 export JAVA_HOME=/usr/lib/jvm/temurin-21-jdk-amd64
 ' >> ~/.profile && source ~/.profile
 
+# install maven
 wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz \
 && tar -xvf apache-maven-3.9.9-bin.tar.gz \
 && sudo mv apache-maven-3.9.9 /opt/ \
@@ -19,3 +31,4 @@ export MAVEN_HOME=/opt/maven
 export PATH=${M2_HOME}/bin:${PATH}
 ' >> ~/.profile \
 && source ~/.profile
+

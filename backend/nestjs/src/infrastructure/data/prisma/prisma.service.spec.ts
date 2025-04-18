09d11a3377d02d0895fe09ad503e1d128372de29
Jérémie Faussion <jeremie.faussion@gmail.com>
2025-04-18 22:07:45 +0200

    Fix test PRisma - Add mock ConfigService to PrismaService tests

--- backend/nestjs/src/infrastructure/data/prisma/prisma.service.spec.ts
+++ backend/nestjs/src/infrastructure/data/prisma/prisma.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('PrismaService', () => {
   let service: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService],
+      providers: [
+        PrismaService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockImplementation((key: string) => {
+              if (key === 'DATABASE_URL') {
+                return 'file:./test.db';
+              }
+              return undefined;
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
